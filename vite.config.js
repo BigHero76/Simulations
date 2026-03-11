31dea43b9c852ca59a15d4a83b00430569cf33c4
Karthikeya <shattererofdreams7@gmail.com>
2026-03-11 23:51:04 +0530

    chore: Increase Vite chunk size warning limit to 1000kB

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   server: {
     host: '127.0.0.1',
     port: 8888,
